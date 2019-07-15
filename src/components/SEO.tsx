b5e9caf9c3a561b321690a63101ad096ac04b3fa
partyKyoung <kyoungah@kyoungah.com>
2019-07-15 16:17:24 +0900

    seo 데이터 수정

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -10,48 +10,49 @@ import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 interface SEOProps {
-  description: string;
-  title: string;
-  url: string;
+  description: string
+  title: string
+  url: string
 }
 
 const SEO: React.SFC<SEOProps> = ({ description, title, url }) => {
   return (
-    <Helmet 
-      title={title}
-    >
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <Helmet title={title}>
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-    <meta name="description" content={description} />
+      <meta name="description" content={description} />
 
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    <meta name="twitter:creator" content="kyoungah" />
-    <meta name="twitter:site" content="@kyoungah8D" />
-    <meta name="twitter:url" content={url} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta
-      name="twitter:image"
-      content="https://dev.kyoungah.com/static/kyoungah.png"
-    />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:creator" content="kyoungah" />
+      <meta name="twitter:site" content="@kyoungah8D" />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:image"
+        content="https://dev.kyoungah.com/kyoungah.png"
+      />
 
-    <meta property="og:locale" content="ko_kR" />
-    <meta property="og:site_name" content="ahn.heejong" />
-    <meta property="og:title" title={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:image" content="https://dev.kyoungah.com/static/kyoungah.png" />
-    <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="376" />
-    <meta property="og:image:height" content="63" />
-    <meta property="og:url" content={url} />
-  </Helmet>
+      <meta property="og:locale" content="ko_kR" />
+      <meta property="og:site_name" content="경아 개발 블로그" />
+      <meta property="og:title" title={title} />
+      <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        content="https://dev.kyoungah.com/kyoungah.png"
+      />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="376" />
+      <meta property="og:image:height" content="63" />
+      <meta property="og:url" content={url} />
+    </Helmet>
   )
-};
+}
 
 SEO.defaultProps = {
   description: ``,
-  title: '',
-  url: ''
+  title: "",
+  url: "",
 }
 
 export default SEO
