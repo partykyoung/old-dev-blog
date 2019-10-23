f9c2146f1f87eeb783e58f043630a9f8e8a1230f
partykyoung <kyoungah@kyoungah.com>
2019-10-24 01:40:14 +0900

    feat: 404 페이지 퍼블리싱 완료

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import { Link } from "gatsby";
 
-const NotFound = () => {
+import styled from "../styledComponents";
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const NotFound = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+const HomeButton = styled(Link)`
+  display: block;
+  margin-top: 1rem;
+  padding: 1rem;
+`;
+
+const NotFoundPage = () => {
   return (
-    <div>
-      앗, 없는 페이지 입니다. X_X
-      <Link to="/">첫 페이지로 돌아가기</Link>
-    </div>
+    <Wrapper>
+      <NotFound>
+        앗, 없는 페이지 입니다. X_X
+        <br/>
+        <HomeButton to="/">첫 페이지로 돌아가기</HomeButton>
+      </NotFound>
+    </Wrapper>
   )
 }
 
-export default NotFound;
+export default NotFoundPage;
