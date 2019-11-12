15e41c7cc837aa4aca071a7292cdd822ca7eaa26
partykyoung <kyoungah@kyoungah.com>
2019-11-12 23:56:41 +0900

    imp: 블로그 페이지 css 수정 중

--- src/templates/blogTemplate.tsx
+++ src/templates/blogTemplate.tsx
@@ -8,7 +8,6 @@ import "katex/dist/katex.min.css"
 import theme from '../styles/theme';
 
 import Layout from '../components/Layout';
-import Comment from '../components/Comment';
 import SEO from '../components/SEO';
 
 const PostHeader = styled.div`
@@ -33,20 +32,19 @@ const PostArticle = styled.article`
   h2, h3, h4, h5, h6 {
     margin-top: 2.625rem;
     margin-bottom: 0.5rem;
+    font-weight: 600;
   }
 
   h2 {
     font-size: 1.5rem;
-    font-weight: 600;
   }
 
   h3 {
     font-size: 1.25rem;
-    font-weight: 500;
   }
 
-  h4 {
-    font-weight: 400;
+  h4, h5, h6 {
+    font-weight: 500;
   }
 
   ul {
@@ -64,6 +62,32 @@ const PostArticle = styled.article`
        list-style-type: circle;
      }
   }
+
+  table {
+    width: 100%;
+  }
+
+  thead {
+    th, td {
+      border-width: 0 0 2px;
+    }
+  }
+
+  tbody {
+    tr:last-child {
+      th, td {
+        border-bottom-width: 0;
+      }
+    }
+  }
+
+  th, td {
+    padding: 0.5rem 0.75rem;
+    border: 1px solid ${theme.gray};
+    border-width: 0 0 1px;
+    vertical-align: middle;
+  }
+}
 
   figcaption {
     color: ${theme.gray};
