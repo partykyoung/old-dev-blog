b93150b1fd01b117b977c4040c698dd9371fca25
partykyoung <kyoungah@kyoungah.com>
2019-12-22 23:56:02 +0900

    feat: 새로운 포스트 draft 폴더에 생성해주는 모듈 작성 중

--- cli/create-new-post.js
+++ cli/create-new-post.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 const { DateTime } = require("luxon");
+const inquirer = require('inquirer');
 
-function getTitle() {
-  return '';
+async function getTitle() {
+  const { title } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: '포스트 타이틀을 입력해주세요.',
+    }
+  ])
+
+  return title;
 }
 
 function getCategory() {
@@ -14,18 +23,24 @@ function getDate() {
 
   return {
     summaryDate: location.toFormat('yyyy-MM-dd'),
-    detailDate: location.toFormat('yyyy-MM-dd HH:mm:ss ')
+    detailDate: location.toFormat('yyyy-MM-dd HH:mm:ss')
   };
 }
 
-function createNewPost() {
-  const title = getTitle();
+function getContent(title, date, category) {
+  return `---\ntitle: ${title}\ndate: ${date}\ncategory: ${category}\ndescription:\ntags:\n---`;
+}
+
+async function createNewPost() {
+  const cwd = process.cwd();
+  const title = await getTitle();
   const category = getCategory();
   const date = getDate();
+  const content = getContent(title, date.detailDate, category);
 
-  fs.writeFile(`${date.summaryDate}-${category}-${title}.md`, 'tt', (err) => {
+  fs.writeFile(`${cwd}/src/posts/${date.summaryDate}-${category}-${title}.md`, content, (err) => {
     if (err) {
-      console.log('error: 포스트 생성에 실패하였습니다. X_X');
+      console.log(err, 'error: 포스트 생성에 실패하였습니다. X_X');
 
       return;
     }
