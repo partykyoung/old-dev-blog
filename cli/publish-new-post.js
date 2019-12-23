6731eb15c28f52b6e0fd59f98e351bff2087b0df
partykyoung <kyoungah@kyoungah.com>
2019-12-23 17:15:14 +0900

    feat: 작성 완료된 포스트 draft 폴더에서 posts 폴더로 옮겨주는 모듈 작성 완료 했으나 코드 정리 필요.

---
+++ cli/publish-new-post.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+async function getPost() {
+  const { post } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'post',
+      message: '발행할 포스트 파일명을 입력해주세요.',
+    }
+  ]);
+
+  return post;
+}
+
+
+async function publishNewPost() {
+  const cwd = process.cwd();
+  const post = await getPost();
+
+  const draftPost = `${cwd}/draft/${post}`;
+  const publishPost = `${cwd}/src/posts/${post}`;
+
+  fs.rename(draftPost, publishPost, (error) => {
+    if (error) {
+      console.log('발행 실패. 파일명에 오타가 있는지 확인해주세요 X_X.');
+
+      return;
+    }
+
+    console.log('발생 완료!');
+  });
+}
+
+module.exports = publishNewPost();
